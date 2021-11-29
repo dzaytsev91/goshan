7aec87113f83421d1b6fc25b0d269d22a6b7b215
Logovskii Dmitrii <d-zaytsev@yandex-team.ru>
2021-11-29 20:40:09 +0300

    rebase

--- main.go
+++ main.go
@@ -5,24 +5,25 @@ import (
 	"log"
 	"net/http"
 )
+
 func hello(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		 http.ServeFile(w, r, "form.html")
+		http.ServeFile(w, r, "form.html")
 	case "POST":
-	    fmt.Printf("%v\n", r)
-	    fmt.Printf("tried password: %s\n", r.FormValue("password"))
+		fmt.Printf("%v\n", r)
+		fmt.Printf("tried password: %s\n", r.FormValue("password"))
 		if err := r.ParseForm(); err != nil {
 			fmt.Fprintf(w, "ParseForm() err: %v", err)
 			return
 		}
 		if r.FormValue("password") == "ty pidor" {
-		    http.ServeFile(w, r, "image.html")
+			http.ServeFile(w, r, "image.html")
 		} else if r.FormValue("password") == "typidor" {
-		    fmt.Fprintf(w, "It was close!")
-        } else {
-            fmt.Fprintf(w, "Wrong password!")
-        }
+			fmt.Fprintf(w, "It was close!")
+		} else {
+			fmt.Fprintf(w, "Wrong password!")
+		}
 	default:
 		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
 	}
