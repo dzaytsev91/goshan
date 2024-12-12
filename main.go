c7b003e17a0696c30dfd390e746adc3ebe9e9737
dlogovskiy <dlogovskiy@ozon.ru>
2024-12-12 16:13:44 +0300

    tasks handler

--- main.go
+++ main.go
@@ -1,11 +1,46 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"strings"
 )
+
+type Task struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	IsCompleted bool   `json:"isCompleted"`
+}
+
+var tasks = []Task{
+	{
+		ID:          "28ba250e-9c11-479f-bcdd-a263cfbd02f0",
+		Title:       "First task to do from server",
+		IsCompleted: false,
+	},
+	{
+		ID:          "e86444e1-d34e-4ab4-853d-9da9f1d4ae7d",
+		Title:       "Second task to do from server",
+		IsCompleted: false,
+	},
+	{
+		ID:          "04ae8534-db50-49bb-88e1-90b58620a2eb",
+		Title:       "Third task to do from server",
+		IsCompleted: false,
+	},
+}
+
+func tasksHandler(w http.ResponseWriter, r *http.Request) {
+	data, err := json.Marshal(tasks)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.Write(data)
+	return
+}
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
@@ -41,6 +76,7 @@ func hello(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", hello)
+	http.HandleFunc("/tasks", tasksHandler)
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
 
 	fmt.Printf("Starting server\n")
