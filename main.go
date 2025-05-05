4f1989ec7ace3a10d439cac819d3b86761e92d94
dlogovskiy <dlogovskiy@ozon.ru>
2025-05-05 09:41:47 +0300

    log any request

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
-	"strings"
 )
 
 type Task struct {
@@ -45,41 +45,58 @@ func tasksHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
+	// Prepare log data
+	fmt.Println("______________________________________________")
+	fmt.Printf("%v\n", r)
+	fmt.Printf("Method: %s\n", r.Method)
+	fmt.Printf("Headers: %s\n", r.Header)
+	fmt.Printf("URL: %s\n", r.URL.String())
 	switch r.Method {
 	case "GET":
-		http.ServeFile(w, r, "form.html")
+		w.Write([]byte("Success"))
 	case "POST":
-		if err := r.ParseForm(); err != nil {
-			fmt.Fprintf(w, "ParseForm() err: %v", err)
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Printf("Error reading body: %v", err)
+			http.Error(w, "can't read body", http.StatusBadRequest)
 			return
 		}
-		story := r.FormValue("story")
-		geogess := r.FormValue("geogess")
-		log.Printf("story input: %s, geogess: %s", story, geogess)
-		var story_pass, geogess_pass bool
-		if story == "26270" {
-			story_pass = true
-		}
-		if strings.ToLower(strings.ReplaceAll(geogess, " ", " ")) == "вандаркхолм" {
-			geogess_pass = true
-		}
 
-		if story_pass && geogess_pass {
-			http.ServeFile(w, r, "image.html")
-		} else {
-			errorMsg := fmt.Sprintf("First password: %t\nSecond: password: %t\n", story_pass, geogess_pass)
-			fmt.Printf(errorMsg)
-			fmt.Fprintf(w, errorMsg)
-		}
+		// Log the raw body
+		log.Printf("POST body: %s\n", string(body))
+		w.Write([]byte("Success"))
+		//if err := r.ParseForm(); err != nil {
+		//	fmt.Fprintf(w, "ParseForm() err: %v", err)
+		//	return
+		//}
+		//story := r.FormValue("story")
+		//geogess := r.FormValue("geogess")
+		//log.Printf("story input: %s, geogess: %s", story, geogess)
+		//var story_pass, geogess_pass bool
+		//if story == "26270" {
+		//	story_pass = true
+		//}
+		//if strings.ToLower(strings.ReplaceAll(geogess, " ", " ")) == "вандаркхолм" {
+		//	geogess_pass = true
+		//}
+		//
+		//if story_pass && geogess_pass {
+		//	http.ServeFile(w, r, "image.html")
+		//} else {
+		//	errorMsg := fmt.Sprintf("First password: %t\nSecond: password: %t\n", story_pass, geogess_pass)
+		//	fmt.Printf(errorMsg)
+		//	fmt.Fprintf(w, errorMsg)
+		//}
 	default:
 		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
 	}
+	fmt.Println("______________________________________________")
 }
 
 func main() {
 	http.HandleFunc("/", hello)
-	http.HandleFunc("/tasks", tasksHandler)
-	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
+	//http.HandleFunc("/tasks", tasksHandler)
+	//http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
 
 	fmt.Printf("Starting server\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
